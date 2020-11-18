2a27fd87c8380fa7f62d554c5460206c0daf2d1d
unknown <bryanovicaustenove@gmail.com>
2020-11-19 00:14:51 +0300

    home page essetials

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ import "assets/css/custom.css";
 import "assets/scss/paper-kit.scss?v=1.2.0";
 import "assets/demo/demo.css?v=1.2.0";
 // pages
-import Main from "views/Main";
-ReactDOM.render(<Main />, document.getElementById("root"));
+import App from "views/Main";
+ReactDOM.render(<App />, document.getElementById("root"));
